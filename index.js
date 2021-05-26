d23ba10c2079d789bcbb57557520802acfb5bf18
Joseph Grainger <grrainger7@gmail.com>
2021-05-26 13:25:57 +0200

    finished :D

---
+++ index.js
@@ -0,0 +1,55 @@
+// 1. Make a variable and assign an even number to that variable. Make sure that variable cannot be changed.
+
+const evenNum = 2;
+
+// 2. Make a variable and assign a negative odd number to that variable. Make sure that variable cannot be changed.
+
+const negOddNum = -3;
+
+
+// 3. Make a variable and assign a string to that variable using double quotes. Make sure that variable cannot be changed.
+
+const doubleQuotesString = "This is a string :D";
+
+
+// 4. Make a variable and assign a string to that variable using single quotes. Make sure that variable cannot be changed.
+
+const sinlgeQuotesString = 'This is a string as well :DDD';
+
+// 5. Make a variable and assign a string to that variable using a template literal. Make sure that variable cannot be changed.
+
+const tempLiteralString = `This is a string tooooo`;
+
+// 6. Make a variable and assign an empty string to that variable using a template literal. Make sure that variable *can* be changed.
+
+let emptyTempLitString = ``;
+
+// 7. Make a variable and assign a boolean to that variable using a template literal. Make sure that variable *can* be changed.
+
+
+let booleanTempLit = `${true}`;
+
+console.log(booleanTempLit);
+
+
+// 8. Make a variable and assign a boolean to that variable using a template literal. Make sure that variable can not be changed.
+
+const constBooleanTempLit = `a non-empty string has truthy value`;
+
+console.log(constBooleanTempLit);
+
+
+// 9. Print the `type` of a variable that has a number value.
+
+console.log(evenNum);
+
+// 10. Print the `type` of a variable that has a string value.
+
+console.log(sinlgeQuotesString);
+
+// 11. Print the `type` of a variable that has a boolean value.
+
+console.log(booleanTempLit);
+
+
+
